7ed5e62c21c8d1757497ae3b4784a2fb7bc901ab
HamzaMhadbi <hamza.mhadbi17@gmail.com>
2018-11-29 19:18:34 +0100

    Updates

---
+++ precache-manifest.6be58ffb61fe3bc601c8b92c3da14bcf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec91630b4c011e32fc87",
+    "url": "/simple-side-menu/static/js/runtime~main.ec91630b.js"
+  },
+  {
+    "revision": "0077d64955c168950b43",
+    "url": "/simple-side-menu/static/js/main.0077d649.chunk.js"
+  },
+  {
+    "revision": "3a17bb9cbeca898d697d",
+    "url": "/simple-side-menu/static/js/1.3a17bb9c.chunk.js"
+  },
+  {
+    "revision": "0077d64955c168950b43",
+    "url": "/simple-side-menu/static/css/main.8507f703.chunk.css"
+  },
+  {
+    "revision": "402c4cb390de904923325966f6c39eb2",
+    "url": "/simple-side-menu/index.html"
+  }
+];
